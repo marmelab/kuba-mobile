8c19700dcb9a1918f1dbcb708c99bf35466ad740
Anthony Rimet <anthony.rimet03@gmail.com>
2021-09-20 10:51:52 +0200

    React native starter

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeBaseProvider } from "native-base";
+import React from "react";
+import { HomeScreen } from "./src/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Center, Box } from "native-base";
+import React from "react";
+
+export const HomeScreen = () => {
+  return (
+    <Center flex={1}>
+      <Box>Hello in Kuba mobile game</Box>
+    </Center>
+  );
+};
