af89317505e04b2a75161a4029a8ad4b23a442f2
Alexandre Herbeth <alexandre.herbeth@gmail.com>
2021-09-20 11:33:15 +0200

    add linter

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: ["prettier"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "arrow-body-style": "off",
+      "prefer-arrow-callback": "off",
+    },
+  };
