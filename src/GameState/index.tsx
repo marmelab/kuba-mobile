f42699994b3213741163d3655870df60b90f3723
Anthony Rimet <anthony.rimet03@gmail.com>
2021-09-20 17:57:14 +0200

    Fetch users for game state

--- src/GameState/index.tsx
+++ src/GameState/index.tsx
@@ -3,23 +3,27 @@ import { Center, Spinner, Text, View } from "native-base";
 import React, { useEffect, useState } from "react";
 import { RootStackParamList } from "../../App";
 import Constants from "expo-constants";
-import { Game } from "../interface";
+import { Game, Player } from "../interface";
 import { Board } from "./Board";
 
 type Props = NativeStackScreenProps<RootStackParamList, "GameState">;
 const API_URL = Constants?.manifest?.extra?.API_URL;
 
 export default function GameState({ route }: Props) {
-  const [data, setData] = useState<Game>();
+  const [game, setGame] = useState<Game>();
+  const [players, setPlayer] = useState<Player[]>();
   const [isLoading, setLoading] = useState(true);
-  const [errors, setErrors] = useState({});
   const gameId = route.params?.gameId;
 
-  const getMovies = async () => {
+  const getGame = async () => {
     try {
       const response = await fetch(`${API_URL}/games/${gameId}`);
-      const json = await response.json();
-      setData(json);
+      const json = (await response.json()) as Game;
+      setGame(json);
+      if (json.players) {
+        const ids = json.players.map((pl) => pl.playerNumber);
+        await getUsers(ids);
+      }
     } catch (error) {
       console.error(error);
     } finally {
@@ -27,8 +31,18 @@ export default function GameState({ route }: Props) {
     }
   };
 
+  const getUsers = async (ids: number[]) => {
+    try {
+      const response = await fetch(`${API_URL}/user?filter=${encodeURI(JSON.stringify({id: ids}))}`);
+      const json = (await response.json()).data as Player[];
+      setPlayer(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    getMovies();
+    getGame();
   }, []);
 
   return (
@@ -37,8 +51,8 @@ export default function GameState({ route }: Props) {
         <Spinner size="lg" />
       ) : (
         <View>
-          <Text fontSize="md">Game #{data?.id}</Text>
-          <Board board={data?.board}/>
+          <Text fontSize="md">Game #{game?.id}</Text>
+          <Board board={game?.board} />
         </View>
       )}
     </Center>
--- src/interface.tsx
+++ src/interface.tsx
@@ -2,9 +2,15 @@ export interface Game {
   id: number;
   board: JSON | null;
   currentPlayer: number | null;
-  players: JSON | null;
+  players: Player[] | null;
   directionSelected: string | null;
   marbleClicked: JSON | null;
   hasWinner: boolean;
   started: boolean;
 }
+
+export interface Player {
+  playerNumber: number;
+  marbleColor: number;
+  marblesWon: number[];
+};
