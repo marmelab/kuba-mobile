a863872bee805c0e7492022addd576c08c357f95
Alexandre Herbeth <alexandre.herbeth@gmail.com>
2021-09-21 11:17:03 +0200

    App config
    add .env file
    Change form .json to app.config.ts
    Splashscreen modified

--- app.config.ts
+++ app.config.ts
@@ -1,21 +1,32 @@
-const appName = 'Kuba Mobile';
+import 'dotenv/config';
 
-module.exports = () => {
-  if (process.env.ENVIRONMENT === 'production') {
-    return {
-      name: appName,
-      version: '1.0.0',
-      extra: {
-        API_URL: 'kittens are cool',
-      },
-    };
-  } else {
-    return {
-      name: appName,
-      version: '1.0.0',
-      extra: {
-        API_URL: 'http://192.168.86.189:3000',
-      },
-    };
-  }
+const myValue = 'Kuba Mobile';
+export default {
+  name: myValue,
+  version: '1.0.0',
+  extra: {
+    API_URL: process.env.API_URL,
+  },
+  splash: {
+    image: './assets/splashscreen.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  icon: './assets/icon.png',
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
 };
