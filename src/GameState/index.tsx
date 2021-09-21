f2e1b49504c5dc4a17bfd631a6ba06afbed0ba03
Anthony Rimet <anthony.rimet03@gmail.com>
2021-09-21 15:21:15 +0200

    add error view on gamestate

--- src/GameState/index.tsx
+++ src/GameState/index.tsx
@@ -1,5 +1,6 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import {
+  ArrowBackIcon,
   Box,
   Center,
   CheckIcon,
@@ -12,35 +13,43 @@ import {
   Stack,
   Text,
   View,
-} from "native-base";
-import React, { useEffect, useReducer } from "react";
-import { RootStackParamList } from "../../App";
-import Constants from "expo-constants";
-import { Game, User } from "../interface";
-import { Board } from "./Board";
-import { Marble } from "./Marble";
+} from 'native-base';
+import React, { useEffect, useReducer } from 'react';
+import { RootStackParamList } from '../../App';
+import Constants from 'expo-constants';
+import { Game, User } from '../interface';
+import { Board } from './Board';
+import { Marble } from './Marble';
+import { background } from 'styled-system';
 
-type Props = NativeStackScreenProps<RootStackParamList, "GameState">;
+type Props = NativeStackScreenProps<RootStackParamList, 'GameState'>;
 const API_URL = Constants?.manifest?.extra?.API_URL;
 
 const initialState: {
   players: User[];
   game: Game | undefined;
   isLoading: boolean;
+  error: any | undefined;
 } = {
   players: [],
   game: undefined,
   isLoading: true,
+  error: undefined,
 };
 
 const reducer = (
-  state: { players: User[]; game: Game | undefined; isLoading: boolean },
-  action: { type: string; value: any }
+  state: {
+    players: User[];
+    game: Game | undefined;
+    isLoading: boolean;
+    error: any | undefined;
+  },
+  action: { type: string; value: any },
 ) => {
   return { ...state, [action.type]: action.value };
 };
 
-export default function GameState({ route }: Props) {
+export default function GameState({ navigation, route }: Props) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const gameId = route.params?.gameId;
 
@@ -48,25 +57,26 @@ export default function GameState({ route }: Props) {
     try {
       const response = await fetch(`${API_URL}/games/${gameId}`);
       const json = (await response.json()) as Game;
-      dispatch({ type: "game", value: json });
+      dispatch({ type: 'game', value: json });
       if (json.players) {
         const ids = json.players.map((pl) => pl.playerNumber);
         await getPlayers(ids);
       }
     } catch (error) {
-      console.error(error);
+      console.log(error);
+      dispatch({ type: 'error', value: error });
     } finally {
-      dispatch({ type: "isLoading", value: false });
+      dispatch({ type: 'isLoading', value: false });
     }
   };
 
   const getPlayers = async (ids: number[]) => {
     try {
       const response = await fetch(
-        `${API_URL}/user?filter=${encodeURI(JSON.stringify({ id: ids }))}`
+        `${API_URL}/user?filter=${encodeURI(JSON.stringify({ id: ids }))}`,
       );
       const json = (await response.json()).data as User[];
-      dispatch({ type: "players", value: json });
+      dispatch({ type: 'players', value: json });
     } catch (error) {
       console.error(error);
     }
@@ -74,9 +84,13 @@ export default function GameState({ route }: Props) {
 
   const getPlayerObject = (player: User) => {
     const playerGame = state?.game?.players?.find(
-      (p) => p.playerNumber === player.id
+      (p) => p.playerNumber === player.id,
     );
     return { ...playerGame, ...player };
+  };
+
+  const back = () => {
+    navigation.goBack();
   };
 
   useEffect(() => {
@@ -94,14 +108,40 @@ export default function GameState({ route }: Props) {
         >
           <Spinner size="lg" />
         </View>
-      ) : (
-        <ScrollView p={6} contentContainerStyle={{paddingBottom: 24}}>
+      ) : state.error ? (
+        <ScrollView p={6} contentContainerStyle={{ paddingBottom: 24 }}>
           <GameInfo game={state.game} />
           <Board board={state.game?.board} />
           {state.players.map((player) => {
             return <GameUser user={getPlayerObject(player)} key={player.id} />;
           })}
         </ScrollView>
+      ) : (
+        <View
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+          flex={1}
+        >
+          <Box
+            background="red.700"
+            shadow={1}
+            p={6}
+            bg={{
+              linearGradient: {
+                colors: ['red.400', 'red.100'],
+                start: [0, 0],
+                end: [1, 0],
+              },
+            }}
+            rounded="lg"
+          >
+            <Text bold color="white">
+              An error occurred during the game loading (please try an other ID)
+            </Text>
+            <ArrowBackIcon color="white" onClick={back} />
+          </Box>
+        </View>
       )}
     </Flex>
   );
@@ -115,7 +155,7 @@ function GameInfo(props: any) {
       mb={4}
       bg={{
         linearGradient: {
-          colors: ["cyan.400", "cyan.100"],
+          colors: ['cyan.400', 'cyan.100'],
           start: [0, 0],
           end: [1, 0],
         },
@@ -159,7 +199,7 @@ function GameUser(props: any) {
       mb={4}
       bg={{
         linearGradient: {
-          colors: ["cyan.400", "cyan.100"],
+          colors: ['cyan.400', 'cyan.100'],
           start: [0, 0],
           end: [1, 0],
         },
