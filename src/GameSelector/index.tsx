d45fef3c4d1740e96858e2d23d59a59639570d69
Anthony Rimet <anthony.rimet03@gmail.com>
2021-09-22 11:30:19 +0200

    Show load error message + empty games

--- src/GameSelector/index.tsx
+++ src/GameSelector/index.tsx
@@ -3,11 +3,8 @@ import {
   Box,
   Button,
   Center,
-  FormControl,
   HStack,
-  Input,
   View,
-  VStack,
   Text,
   CheckIcon,
   Flex,
@@ -15,6 +12,8 @@ import {
   Stack,
   ScrollView,
   Heading,
+  Spinner,
+  useToast,
 } from 'native-base';
 import React, { useEffect, useState } from 'react';
 import { API_URL } from '../constant';
@@ -47,6 +46,12 @@ export default function ({ navigation, player }: any) {
       const json = (await response.json()).data as Game[];
       setUserGames(json);
     } catch (error) {
+      const toast = useToast();
+      toast.show({
+        title: 'Error',
+        status: 'error',
+        description: 'The games could not be loaded',
+      });
       setErrorUserGames(error);
     } finally {
       setIsLoading(false);
@@ -69,78 +74,110 @@ export default function ({ navigation, player }: any) {
         <Heading size="lg" color="primary.500" mt={4} mb={4}>
           User games
         </Heading>
-        {userGames?.map((game) => (
+        {!userGames?.length && (
           <Box
-            shadow={3}
-            mb={4}
-            width="full"
+            background="red.700"
+            shadow={1}
+            p={6}
+            bg={{
+              linearGradient: {
+                colors: ['red.400', 'red.100'],
+                start: [0, 0],
+                end: [1, 0],
+              },
+            }}
             rounded="lg"
-            key={game.id}
-            bg="white"
           >
-            <HStack
-              alignItems="center"
-              justifyContent="center"
-              bg={{
-                linearGradient: {
-                  colors: ['cyan.400', 'cyan.100'],
-                  start: [0, 0],
-                  end: [1, 0],
-                },
-              }}
-              rounded="lg"
-            >
-              <Board board={game?.board} preview={true} />
-            </HStack>
-            <HStack
-              alignItems="center"
-              justifyContent="space-between"
-              p={4}
-              space={2}
-            >
-              <Text fontSize="lg" bold color="black">
-                Game #{game?.id}
-              </Text>
-              <Flex alignItems="center" direction="row">
-                <Text fontSize="lg" bold color="black" pr={2}>
-                  Has Winner:
-                </Text>
-                {game?.hasWinner ? (
-                  <CheckIcon color="green.600" />
-                ) : (
-                  <SmallCloseIcon color="red.600" />
-                )}
-              </Flex>
-            </HStack>
-            <Stack p={4} space={2}>
-              {game?.players?.map((player) => (
-                <Text
-                  fontSize="md"
-                  bold
-                  color="black"
-                  key={player?.playerNumber}
-                >
-                  Player #{player?.playerNumber}
-                </Text>
-              ))}
-            </Stack>
-            <Stack>
-              <Button
-                onPress={() => navigateToGameState(game.id)}
-                colorScheme="cyan"
-              >
-                Go !
-              </Button>
-            </Stack>
+            <Text bold color="white">
+              No games was found
+            </Text>
           </Box>
-        ))}
+        )}
+
+        {userGames &&
+          userGames?.map((game) => (
+            <Box
+              shadow={3}
+              mb={4}
+              width="full"
+              rounded="lg"
+              key={game.id}
+              bg="white"
+            >
+              <HStack
+                alignItems="center"
+                justifyContent="center"
+                bg={{
+                  linearGradient: {
+                    colors: ['cyan.400', 'cyan.100'],
+                    start: [0, 0],
+                    end: [1, 0],
+                  },
+                }}
+                rounded="lg"
+              >
+                <Board board={game?.board} preview={true} />
+              </HStack>
+              <HStack
+                alignItems="center"
+                justifyContent="space-between"
+                p={4}
+                space={2}
+              >
+                <Text fontSize="lg" bold color="black">
+                  Game #{game?.id}
+                </Text>
+                <Flex alignItems="center" direction="row">
+                  <Text fontSize="lg" bold color="black" pr={2}>
+                    Has Winner:
+                  </Text>
+                  {game?.hasWinner ? (
+                    <CheckIcon color="green.600" />
+                  ) : (
+                    <SmallCloseIcon color="red.600" />
+                  )}
+                </Flex>
+              </HStack>
+              <Stack p={4} space={2}>
+                {game?.players?.map((player) => (
+                  <Text
+                    fontSize="md"
+                    bold
+                    color="black"
+                    key={player?.playerNumber}
+                  >
+                    Player #{player?.playerNumber}
+                  </Text>
+                ))}
+              </Stack>
+              <Stack>
+                <Button
+                  onPress={() => navigateToGameState(game.id)}
+                  colorScheme="cyan"
+                >
+                  Go !
+                </Button>
+              </Stack>
+            </Box>
+          ))}
       </ScrollView>
     );
   };
 
   return (
     <Center flex={1}>
-      <UserGamesList />
+      {isLoading ? (
+        <View
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+          flex={1}
+        >
+          <Spinner size="lg" />
+        </View>
+      ) : (
+        <UserGamesList />
+      )}
     </Center>
   );
 }
