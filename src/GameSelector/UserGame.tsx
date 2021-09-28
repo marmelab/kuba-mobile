89eea675823a58c15e2cc9d814f39343914f15d0
Anthony Rimet <anthony.rimet03@gmail.com>
2021-09-28 10:45:56 +0200

    Display board via absolute position

--- src/GameSelector/UserGame.tsx
+++ src/GameSelector/UserGame.tsx
@@ -3,25 +3,12 @@ import React from 'react';
 import { Pressable } from 'react-native';
 import { Board } from '../GameState/Board';
 import { Game } from '../interface';
+import { GRADIENT_COLORS } from './gradientColors';
 
 interface UserGameProps {
   navigateToGameState: (id: number | undefined) => void;
   game: Game | undefined;
 }
-
-const GRADIENT_COLORS: any = [];
-GRADIENT_COLORS[0] = {
-  background: ['orange.400', 'amber.400'],
-  foreground: 'black',
-};
-GRADIENT_COLORS[1] = {
-  background: ['emerald.400', 'lime.400'],
-  foreground: 'black',
-};
-GRADIENT_COLORS[2] = {
-  background: ['error.600', 'danger.300'],
-  foreground: 'black',
-};
 
 export const UserGame = ({ game, navigateToGameState }: UserGameProps) => {
   const getGradientColor = (bool: boolean) => {
---
+++ src/GameSelector/gradientColors.tsx
@@ -0,0 +1,13 @@
+export const GRADIENT_COLORS: any = [];
+GRADIENT_COLORS[0] = {
+  background: ['orange.400', 'amber.400'],
+  foreground: 'black',
+};
+GRADIENT_COLORS[1] = {
+  background: ['emerald.400', 'lime.400'],
+  foreground: 'black',
+};
+GRADIENT_COLORS[2] = {
+  background: ['error.600', 'danger.300'],
+  foreground: 'black',
+};
