c7120a6e5f6a8a74783e7d22c20d1a42c4b8d0e2
Anthony Rimet <anthony.rimet03@gmail.com>
2021-09-29 10:22:05 +0200

    Add board in png

--- src/GameState/Board.tsx
+++ src/GameState/Board.tsx
@@ -1,12 +1,13 @@
 import { Box, View } from 'native-base';
 import React from 'react';
+import { ImageBackground } from 'react-native';
 import { BOARD_GREEN } from './boardColors';
 import { convertBoardToBoardCoordinate } from './convertBoardToBoardCoordinate';
 import { Marble } from './Marble';
 
 export const Board = (props: any) => {
   const board = props.board;
-  const boardSize = props.preview ? '128px' : '360px';
+  const boardSize = props.preview ? '128px' : '364px';
   const marbleColorSize = props.preview ? '16px' : '48px';
   const marbleEmptySize = props.preview ? '8px' : '16px';
 
@@ -18,39 +19,46 @@ export const Board = (props: any) => {
   const boxMarbleSizePourcent = `${boxMarbleSize}%`;
 
   return (
-    <View
-      position="relative"
-      width={boardSize}
-      height={boardSize}
-      bg="cyan.500"
-    >
-      {boardCoordinate.map((item) => (
-        <Box
-          alignItems="center"
-          justifyContent="center"
-          key={item.x + ',' + item.y}
-          position="absolute"
-          left={`${item.x * boxMarbleSize}%`}
-          top={`${item.y * boxMarbleSize}%`}
-          width={boxMarbleSizePourcent}
-          height={boxMarbleSizePourcent}
-          bg={
-            marbleClickedCoordinates?.x === item.x &&
-            marbleClickedCoordinates?.y === item.y
-              ? BOARD_GREEN
-              : ''
-          }
-        >
-          <Marble
-            value={item.value}
-            size={item.value === 0 ? marbleEmptySize : marbleColorSize}
-            rowIndex={item.y}
-            cellIndex={item.x}
-            setMarbleClickedCoordinates={setMarbleClickedCoordinates}
-            setMarbleClicked={props.setMarbleClicked}
-          />
-        </Box>
-      ))}
+    <View width={boardSize} height={boardSize}>
+      <ImageBackground
+        source={require('./board.png')}
+        resizeMode="cover"
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {boardCoordinate.map((item) => (
+          <Box
+            alignItems="center"
+            justifyContent="center"
+            key={item.x + ',' + item.y}
+            position="absolute"
+            left={`${item.x * boxMarbleSize}%`}
+            top={`${item.y * boxMarbleSize}%`}
+            width={boxMarbleSizePourcent}
+            height={boxMarbleSizePourcent}
+            bg={
+              marbleClickedCoordinates?.x === item.x &&
+              marbleClickedCoordinates?.y === item.y
+                ? BOARD_GREEN
+                : ''
+            }
+          >
+            {item.value != 0 && (
+              <Marble
+                value={item.value}
+                size={marbleColorSize}
+                rowIndex={item.y}
+                cellIndex={item.x}
+                setMarbleClickedCoordinates={setMarbleClickedCoordinates}
+                setMarbleClicked={props.setMarbleClicked}
+              />
+            )}
+          </Box>
+        ))}
+      </ImageBackground>
     </View>
   );
 };
